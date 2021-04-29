ba870bfc33c937e0404074b512f675f020eb77b6
Dascal Eugeniu <evgenii.dascal@gmail.com>
2021-04-29 14:09:23 +0300

    Спринт 7: Изменил состояние кпопки после добавления карточки

--- script/index.js
+++ script/index.js
@@ -42,71 +42,9 @@ editProfileForm.addEventListener('submit', saveProfile);
 
 // ----------------
 
-// Add Card Popup
-
-const addPlacePopup = document.querySelector('.popup_type_add-place');
-const addPlaceForm = document.forms.addPlaceForm;
-const placeName = addPlaceForm.elements.placeName;
-const placeLink = addPlaceForm.elements.placeLink;
-
-addPlaceBtn.addEventListener('click', () => {
-  openPopup(addPlacePopup);
-});
-
-function savePlace(e) {
-  e.preventDefault();
-
-  const name = placeName.value;
-  const link = placeLink.value;
-
-  addCard(name, link);
-
-  closePopup(addPlacePopup);
-  addPlaceForm.reset();
-}
-
-const cardValidation = new FormValidator(addPlaceForm, {
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__btn-save',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__input-error_active',
-});
-
-cardValidation.enableValidation();
-
-addPlaceForm.addEventListener('submit', savePlace);
-
-//----------------
-
 // Create Card
 
 const card = document.querySelector('.card');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
 
 function createCard(name, link) {
   const cardElement = new Card(name, link);
@@ -123,10 +61,48 @@ function addCard(name, link) {
   renderCard(newCard);
 }
 
+// Add Card Popup
+
+const addPlacePopup = document.querySelector('.popup_type_add-place');
+const addPlaceForm = document.forms.addPlaceForm;
+const placeName = addPlaceForm.elements.placeName;
+const placeLink = addPlaceForm.elements.placeLink;
+const addPlaceFormSaveBtn = addPlacePopup.querySelector('.popup__btn-save');
+
+addPlaceBtn.addEventListener('click', () => {
+  openPopup(addPlacePopup);
+});
+
+function savePlace(e) {
+  e.preventDefault();
+
+  const name = placeName.value;
+  const link = placeLink.value;
+
+  addCard(name, link);
+
+  closePopup(addPlacePopup);
+  addPlaceForm.reset();
+  addPlaceFormSaveBtn.setAttribute('disabled', true);
+}
+
+const cardValidation = new FormValidator(addPlaceForm, {
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__btn-save',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active',
+});
+
+cardValidation.enableValidation();
+
+addPlaceForm.addEventListener('submit', savePlace);
+
+//----------------
+
+//---------------
+
 document.addEventListener('DOMContentLoaded', () => {
   initialCards.forEach((card) => {
     addCard(card.name, card.link);
   });
 });
-
-//---------------
